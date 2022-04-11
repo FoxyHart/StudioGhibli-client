32ee0e32cd880dd3008737ceefc09e9fb34f391b
FoxyHart <88792549+FoxyHart@users.noreply.github.com>
2022-04-11 11:13:20 -0400

    3.8
    Tryin switching BrowerRouter to HashRouter

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 //#0
 import { setMovies } from '../../actions/actions';
